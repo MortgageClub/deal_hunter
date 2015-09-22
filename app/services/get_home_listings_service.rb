e3c351a74a09246fe338414a5f2354b80ba70666
Cuong Vu <cuongvu0103@gmail.com>
2015-09-22 16:54:36 +0700

    We crawled data from Metro List

--- app/services/get_home_listings_service.rb
+++ app/services/get_home_listings_service.rb
@@ -17,40 +17,56 @@ class GetHomeListingsService
     @session.execute_script("$('#j_username').val('01972404')")
     @session.execute_script("$('#password').val('Blackdawn1')")
     @session.execute_script("$('#j_password').val('Blackdawn1')")
-    # @session.execute_script("$('form#loginform').submit()")
     @session.click_on("SIGN IN")
     sleep(8)
-    # @session.execute_script("$('#password').val(Blackdawn1)")
-    #
-    # sleep(10)
-    # @session.find('#login').native.send_keys(:return)
-
-    # fill in form
   end
 
   def self.go_to_metro_list
     @session.click_link("MainContent_OtherMLS1_rpOtherApplications_SACM_3")
     @session.visit("http://qtrosso.rapmls.com/sp/startSSO.ping?PartnerIdpId=MLSListingsIdentityProvider&TargetResource=http://login.rapmls.com/SACM/','SACM'")
     @session.execute_script("$('#btnContinue').trigger('click')")
-    byebug
+    sleep(5)
     session_id = @session.current_url.split("&SID=").last
-    byebug
-    # @session.visit("http://search.metrolist.net/PropertyTypeTab.aspx?hidMLS=SACM&DTYPE=HS&SRID=211378545&STYPE=HS&DCODE=&SNAME=Sac+under+160k&SID=#{session_id}")
-    # @session.visit("http://search.metrolist.net/Search.aspx?hidMLS=SACM&Action=7&SearchRID=211378545&SessionNumber=182966228&ProspectName=&PropspectRID=&SearchName=Sac+under+160k&SearchType=HS&SID=#{session_id}")
-    @session.visit("http://search.metrolist.net/ListingGridDisplay.aspx?hidMLS=SACM&GRID=137130&PTYPE=RESI&SRC=HS&SRID=211378545&PRINT=0&SAS=0&ARCH=0&HIDD=0&REMO=0&SNAME=Sac+under+160k&CARTID=&SPLISTINGRID=0&STYPE=HS&SID=#{session_id}")
-    sleep(10)
-    byebug
-    @session.execute_script("SetListings()");
-    byebug
+    count = 0
     data = Nokogiri::HTML.parse(@session.html)
+    while data.css(".subject-list-grid").empty? && count < 5
+      @session.visit("http://search.metrolist.net/ListingGridDisplay.aspx?hidMLS=SACM&GRID=137130&PTYPE=RESI&SRC=HS&SRID=211618742&PRINT=0&SAS=0&ARCH=0&HIDD=0&REMO=0&SNAME=Sacramento+Hotsheet&CARTID=&SPLISTINGRID=0&STYPE=HS&SID=#{session_id}")
+      data = Nokogiri::HTML.parse(@session.html)
+      count += 1
+      sleep(5)
+    end
+    data = Nokogiri::HTML.parse(@session.html)
+    count = 0
+    result = []
+    table = data.css(".subject-list-grid")
+    table.css("tr").each do |tr|
+      count += 1
+      p count
+      next if tr.css("td").size < 2
+
+      address = tr.css("td")[4].text
+      city = tr.css("td")[5].text
+      zipcode = tr.css("td")[6].text
+      price = tr.css("td")[7].text
+      agent_name = tr.css("td")[12].text
+      agent_email = tr.css("td")[13].text
+      agent_phone = tr.css("td")[14].text
+      office_name = tr.css("td")[15].text
+
+      result << {
+        price: price, address: address, city: city, zipcode: zipcode,
+        office_name: office_name, agent_name: agent_name, agent_phone: agent_phone, agent_email: agent_email
+      }
+    end
     byebug
   end
 
   def self.set_up_crawler
     Capybara.register_driver :poltergeist do |app|
-      Capybara::Poltergeist::Driver.new(app, {js_errors: false})
+      Capybara::Poltergeist::Driver.new(app, {js_errors: false, timeout: 60})
     end
     @session = Capybara::Session.new(:poltergeist)
+    @session.driver.headers = { "User-Agent" => "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36" }
     # @session = Capybara::Session.new(:selenium)
   end
 end
