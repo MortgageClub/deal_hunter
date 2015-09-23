3c674b08616e8c67b63c8f4d7e637e5e16da716e
Cuong Vu <cuongvu0103@gmail.com>
2015-09-23 09:30:09 +0700

    Format data & refactor code

--- app/services/get_home_listings_service.rb
+++ app/services/get_home_listings_service.rb
@@ -8,6 +8,7 @@ class GetHomeListingsService
     set_up_crawler
     login
     go_to_metro_list
+    crawl_data
   end
 
   private
@@ -18,7 +19,7 @@ class GetHomeListingsService
     @session.execute_script("$('#password').val('Blackdawn1')")
     @session.execute_script("$('#j_password').val('Blackdawn1')")
     @session.click_on("SIGN IN")
-    sleep(8)
+    sleep(10)
   end
 
   def self.go_to_metro_list
@@ -26,6 +27,9 @@ class GetHomeListingsService
     @session.visit("http://qtrosso.rapmls.com/sp/startSSO.ping?PartnerIdpId=MLSListingsIdentityProvider&TargetResource=http://login.rapmls.com/SACM/','SACM'")
     @session.execute_script("$('#btnContinue').trigger('click')")
     sleep(5)
+  end
+
+  def self.crawl_data
     session_id = @session.current_url.split("&SID=").last
     count = 0
     data = Nokogiri::HTML.parse(@session.html)
@@ -35,30 +39,41 @@ class GetHomeListingsService
       count += 1
       sleep(5)
     end
+
+    return if data.css(".subject-list-grid").empty?
+
     data = Nokogiri::HTML.parse(@session.html)
-    count = 0
     result = []
     table = data.css(".subject-list-grid")
     table.css("tr").each do |tr|
-      count += 1
-      p count
       next if tr.css("td").size < 2
 
+      listing_id = tr.css("td")[1].text
       address = tr.css("td")[4].text
       city = tr.css("td")[5].text
       zipcode = tr.css("td")[6].text
-      price = tr.css("td")[7].text
-      agent_name = tr.css("td")[12].text
+      price = tr.css("td")[7].text.gsub(/[^0-9\.]/,'')
+      full_name = tr.css("td")[12].text.strip
+      first_name = full_name.split(" ").first
+      last_name = full_name.split(" ").last
       agent_email = tr.css("td")[13].text
       agent_phone = tr.css("td")[14].text
       office_name = tr.css("td")[15].text
 
       result << {
-        price: price, address: address, city: city, zipcode: zipcode,
-        office_name: office_name, agent_name: agent_name, agent_phone: agent_phone, agent_email: agent_email
+        listing_id: listing_id, price: price, address: address,
+        city: city, zipcode: zipcode,
+        agent: {
+          full_name: full_name,
+          first_name: first_name,
+          last_name: last_name,
+          phone: agent_phone,
+          email: agent_email,
+          office_name: office_name
+        }
       }
     end
-    byebug
+    result
   end
 
   def self.set_up_crawler
@@ -70,3 +85,36 @@ class GetHomeListingsService
     # @session = Capybara::Session.new(:selenium)
   end
 end
+
+# [
+  # [19] {
+  #     :listing_id => "15049016",
+  #          :price => "160000",
+  #        :address => "1525 Katharine Ave",
+  #           :city => "Sacramento",
+  #        :zipcode => "95838",
+  #          :agent => {
+  #           :full_name => "Ron D Allen",
+  #          :first_name => "Ron",
+  #           :last_name => "Allen",
+  #               :phone => "916-807-1210",
+  #               :email => "drallenifbc@yahoo.com",
+  #         :office_name => "Ron Allen & Associates Real Estate"
+  #     }
+  # },
+  # [20] {
+  #     :listing_id => "15054201",
+  #          :price => "151900",
+  #        :address => "5626 Revelstok Dr",
+  #           :city => "Sacramento",
+  #        :zipcode => "95842",
+  #          :agent => {
+  #           :full_name => "Michael J Taylor",
+  #          :first_name => "Michael",
+  #           :last_name => "Taylor",
+  #               :phone => "916-878-1754",
+  #               :email => "mike@intrustrg.com",
+  #         :office_name => "HomeSmart Intrust Realty Group"
+  #     }
+  # }
+# ]
