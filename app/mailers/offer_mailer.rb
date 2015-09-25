189d739a66ccfce5a7db1fd47e07326710d8ba10
Cuong Vu <cuongvu0103@gmail.com>
2015-09-25 09:25:28 +0700

    Update email's template

--- app/mailers/offer_mailer.rb
+++ app/mailers/offer_mailer.rb
@@ -1,10 +1,10 @@
 class OfferMailer < ActionMailer::Base
   default :from => ENV['EMAIL_SENDER']
 
-  def notify_agent(name, email, address)
+  def notify_agent(name, email, address, city)
     @agent_email = email
     @agent_name = name
-    @property_address = address
+    @property_address = address + ', ' + city
     mail(
       to: email,
       subject: "Offer for #{@property_address}"
