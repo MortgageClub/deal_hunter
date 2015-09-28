340b5548cc10e80c58b88e71c6eba783e4223014
Cuong Vu <cuongvu0103@gmail.com>
2015-09-28 11:39:22 +0700

    Move ENV vars to Constants

--- app/services/reply_sms_service.rb
+++ app/services/reply_sms_service.rb
@@ -3,10 +3,14 @@ require 'plivo'
 class ReplySmsService
   include Plivo
 
+  AUTH_ID = ENV["PLIVO_AUTH_ID"]
+  AUTH_TOKEN = ENV["PLIVO_AUTH_TOKEN"]
+  PLIVO_SENDER = ENV["PLIVO_SENDER"]
+
   def self.call(message)
-    plivo = RestAPI.new(ENV["PLIVO_AUTH_ID"], ENV["PLIVO_AUTH_TOKEN"])
+    plivo = RestAPI.new(AUTH_ID, AUTH_TOKEN)
     params = {
-      'src' => ENV["PLIVO_SENDER"],
+      'src' => PLIVO_SENDER,
       'dst' => message.phone_number,
       'text' => message.reply
     }
