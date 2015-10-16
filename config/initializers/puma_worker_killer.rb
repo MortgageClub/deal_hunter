1fc3f3a48ba37c2bf86bc38d4ea0859dab184e02
Cuong Vu <cuongvu0103@gmail.com>
2015-10-16 23:45:39 +0700

    Set percent_usage for Puma Worker Killer

--- config/initializers/puma_worker_killer.rb
+++ config/initializers/puma_worker_killer.rb
@@ -1,1 +1,5 @@
+PumaWorkerKiller.config do |config|
+  config.percent_usage = 0.94
+  config.rolling_restart_frequency = 12 * 3600 # 12 hours in seconds
+end
 PumaWorkerKiller.enable_rolling_restart
