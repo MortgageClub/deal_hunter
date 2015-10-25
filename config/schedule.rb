b19dd88e5870d10fd8209edd1af52bee218b0eb8
Cuong Vu <cuongvu0103@gmail.com>
2015-10-25 10:00:04 +0700

    Update schedule task

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,9 @@
 env :PATH, ENV['PATH']
 
 every 15.minutes do
-  rake "crawler:hunt", :output => './log/hunt.log'
+  rake "crawler:hunt", output: "./log/hunt.log"
 end
 
-every 1.day, at: '1:00 am' do
+every 1.day, at: "1:12 am" do
   rake "db:clear_report"
 end
