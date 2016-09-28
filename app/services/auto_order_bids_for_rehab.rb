4a1f9782ab7bdfcf0ae6b5fad4d9cc6ca6987553
Tang Nguyen <nvtang.khtn@gmail.com>
2016-09-28 11:20:27 +0700

    update auto bids rehab

--- app/services/auto_order_bids_for_rehab.rb
+++ app/services/auto_order_bids_for_rehab.rb
@@ -13,7 +13,7 @@ class AutoOrderBidsForRehab
     result = []
 
     login
-    new_project_chrome
+    new_project
     close_crawler
 
     result
@@ -27,7 +27,7 @@ class AutoOrderBidsForRehab
     sleep(5)
   end
 
-  def new_project_chrome
+  def new_project
     is_error = false
     crawler.visit("https://www.buildzoom.com/project/new")
     sleep(3)
@@ -39,77 +39,86 @@ class AutoOrderBidsForRehab
     end
 
     if is_error
-      #set project name
-      crawler.execute_script("$('#service_request_title').val('Home remodel')")
-      crawler.execute_script("$('#service_request_title').trigger('change')")
-      crawler.execute_script("$('div.title-content button:last').trigger('click')")
-
-      #select time
-      crawler.execute_script("$('div.urgency-content div.answer-button[tabindex=4]').trigger('click')")
-
-      #select bids
-      crawler.execute_script("$('div.expected-responses-content div.answer-button[tabindex=9]').trigger('click')")
-
-      #set address
-      crawler.execute_script("$($('input.project-location')[0]).val('#{listing.address}, #{listing.city}')")
-      crawler.execute_script("$($('input.project-location')[0]).trigger('change')")
-      sleep(3)
-      crawler.execute_script("$($('.uib-typeahead-match')[0]).trigger('click')")
-      crawler.execute_script("$('div.location-content button:last').trigger('click')")
-
-      #select square feet
-      crawler.execute_script("$('div.job-site-content div.answer-button:nth(3)').trigger('click')")
-
-      #select buget
-      crawler.execute_script("$('#budget').val('string:5000-20000')")
-      crawler.execute_script("$('#budget').trigger('change')")
-      crawler.execute_script("$('div.budget-content button:last').trigger('click')")
-
-      #set description
-      crawler.execute_script("$($('div.description-content textarea')[0]).val('Hello, my name is Billy. I am looking for a professional to perform a total home remodel on a home that we recently purchased. The remodel will include interior and exterior paint, new laminate flooring, renovate bathrooms, renovate kitchen, and other miscellaneous items. \\nIf you are interested please contact me. Thanks.')")
-      crawler.execute_script("$($('div.description-content textarea')[0]).trigger('change')")
-
-      crawler.execute_script("$('div.description-content button:last').trigger('click')")
-      sleep(1)
+      new_project_js
     else
-      close_crawler
-      raise Capybara::ElementNotFound
+      new_project_form
+      # close_crawler
+      # raise Capybara::ElementNotFound
     end
   end
 
-  def new_project_firefox
-    crawler.visit("https://www.buildzoom.com/project/new")
-
+  def new_project_js
     #set project name
     crawler.execute_script("$('#service_request_title').val('Home remodel')")
-    crawler.execute_script("$('#service_request_full_name').val('Billy Tran')")
-    crawler.execute_script("$('#project-form-submit').trigger('click')")
+    crawler.execute_script("$('#service_request_title').trigger('change')")
+    crawler.execute_script("$('div.title-content button:last').trigger('click')")
 
     #select time
-    crawler.execute_script("$('#urgency').val('string:Within the next few weeks')")
+    crawler.execute_script("$('div.urgency-content div.answer-button[tabindex=4]').trigger('click')")
+
+    #select bids
+    crawler.execute_script("$('div.expected-responses-content div.answer-button[tabindex=9]').trigger('click')")
+
+    #set address
+    crawler.execute_script("$($('input.project-location')[0]).val('#{listing.address}, #{listing.city}')")
+    crawler.execute_script("$($('input.project-location')[0]).trigger('change')")
+    sleep(3)
+    crawler.execute_script("$($('.uib-typeahead-match')[0]).trigger('click')")
+    crawler.execute_script("$('div.location-content button:last').trigger('click')")
+
+    #select square feet
+    crawler.execute_script("$('div.job-site-content div.answer-button:nth(3)').trigger('click')")
 
     #select buget
     crawler.execute_script("$('#budget').val('string:5000-20000')")
-    crawler.save_screenshot("part1.png")
+    crawler.execute_script("$('#budget').trigger('change')")
+    crawler.execute_script("$('div.budget-content button:last').trigger('click')")
+
+    #set description
+    crawler.execute_script("$($('div.description-content textarea')[0]).val('Hello, my name is Billy. I am looking for a professional to perform a total home remodel on a home that we recently purchased. The remodel will include interior and exterior paint, new laminate flooring, renovate bathrooms, renovate kitchen, and other miscellaneous items. \\nIf you are interested please contact me. Thanks.')")
+    crawler.execute_script("$($('div.description-content textarea')[0]).trigger('change')")
+
+    crawler.execute_script("$('div.description-content button:last').trigger('click')")
+    sleep(1)
+  end
+
+  def new_project_form
+    #set project name
+    crawler.execute_script("$('#service_request_title').val('Home remodel')")
+    crawler.execute_script("$('#service_request_title').trigger('change')")
+    crawler.execute_script("$('#service_request_full_name').val('Billy Tran')")
+    crawler.execute_script("$('#service_request_full_name').trigger('change')")
+    crawler.execute_script("$('#project-form-submit').trigger('click')")
+    sleep(3)
+
+    #select time
+    crawler.execute_script("$('#urgency').val('string:Within the next few weeks')")
+    crawler.execute_script("$('#urgency').trigger('change')")
+
+    #select buget
+    crawler.execute_script("$('#budget').val('string:5000-20000')")
+    crawler.execute_script("$('#budget').trigger('change')")
 
     #set address
-    crawler.execute_script("$('#street_address').val('1673 CHRISTOPHER ST, WINTER GARDEN, FL 34787')")
-    crawler.execute_script("$($('input.project-location')[0]).trigger('change')")
+    crawler.execute_script("$('#street_address').val('#{listing.address}, #{listing.city}')")
+    crawler.execute_script("$('#street_address').trigger('change')")
     sleep(3)
     crawler.execute_script("$($('.uib-typeahead-match')[0]).trigger('click')")
 
     #select square feet
     crawler.execute_script("$('#square_footage').val('2000')")
-    crawler.save_screenshot("part2.png")
+    crawler.execute_script("$('#square_footage').trigger('change')")
+
     #set description
     crawler.execute_script("$('#description').val('Hello, my name is Billy. I am looking for a professional to perform a total home remodel on a home that we recently purchased. The remodel will include interior and exterior paint, new laminate flooring, renovate bathrooms, renovate kitchen, and other miscellaneous items. \nIf you are interested please contact me. Thanks.')")
+    crawler.execute_script("$('#description').trigger('change')")
 
     #select bids
     crawler.execute_script("$('#expected_responses').val('number:4')")
-    crawler.save_screenshot("part3.png")
+    crawler.execute_script("$('#expected_responses').trigger('change')")
 
-    # crawler.execute_script("$('.project-submit-button:last').trigger('click')")
-    sleep(3)
+    crawler.execute_script("$('.project-submit-button:last').trigger('click')")
+    sleep(1)
   end
 
   def set_up_crawler
